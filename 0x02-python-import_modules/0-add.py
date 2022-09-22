331152f905cb7c638ea3122c446b1e7f2835c8b3
Valerie Omondi <valerieomondi5@gmail.com>
2022-09-22 06:56:46 -0700

    Add 2 integers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+
 from add_0 import add
 
-def print_add():
+
+def print_add(a, b):
     a = 1
     b = 2
 
     print("{} + {} = {}".format(a, b, add(a, b)))
-
-if __name__ == '__main__':
-    print_add()
